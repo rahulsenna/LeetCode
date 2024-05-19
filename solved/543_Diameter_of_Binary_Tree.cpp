699d57a4cac213f7c660b51d78138d2bded79cbd
Rahul Senna <rahulvsenna@gmail.com>
2024-05-20 01:35:52 +0530

    543 solved

---
+++ solved/543_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*        
+                    543. Diameter of Binary Tree
+
+                --------------- Approach --------------
+
+1. Depth-First Search (DFS):
+    * DFS
+    * For each node, calculate the height of its left and right subtrees.
+    * diameter =  heights (left + right) subtrees.
+
+2. Update the Maximum Diameter:
+    * Keep track of the maximum diameter encountered during the DFS traversal.
+    
+3. Return the Height:
+    * Return the height of the subtree =  1 + max(leftHeight, rightHeight).
+*/
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int res = 0;
+    int dfs(TreeNode *root){
+        if (!root) return -1;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        res = max(res, left+right+2); // Diameter
+
+        return max(left,right)+1; // Height
+
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
+
+int main()
+{
+    cout << "Hello, Sailor\n";
+
+	return(0);
+}
