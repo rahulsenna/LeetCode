65a860def4a968cd6f0d0c5b830c75a609c9b242
Rahul Senna <rahulvsenna@gmail.com>
2024-06-05 07:32:32 +0530

    yami9's blog Recursion and Backtracking [O]

---
+++ codeforces_solved/O_Fibonacci.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if 1
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+// #define debug(x) cerr << __LINE__ << ": " << #x <<" -> "; _print(x); cerr << '\n';
+#define debug(...) cerr << __LINE__ << ": "; _debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+// #define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _debug() {cerr << '\n';}
+template<typename T, typename... Args>
+void _debug(const T& first, const Args&... args) { cerr << " "; _print(first); cerr << " | "; _debug(args...);}
+#endif 
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    function<int (int)> fib = [&](int n){
+      
+        if (n==1)
+            return 0;
+        if (n<2)
+            return 1;
+        
+ 
+            
+        return  fib(n-1)+fib(n-2);
+        
+        
+    };
+    int n;
+    cin >> n;
+    
+    cout << fib(n);
+ 
+    return 0;
+}
