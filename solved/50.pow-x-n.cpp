d7f86bca239b95d8d921c3fb292e8001885b8e57
Rahul Senna <rahulvsenna@gmail.com>
2024-06-10 09:58:37 +0530

    50 done binary expontiation (LeetCode>

---
+++ solved/50.pow-x-n.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (34.88%)
+ * Likes:    9600
+ * Dislikes: 9417
+ * Total Accepted:    1.7M
+ * Total Submissions: 4.8M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (i.e.,
+ * x^n).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: x = 2.00000, n = 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: x = 2.10000, n = 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: x = 2.00000, n = -2
+ * Output: 0.25000
+ * Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * -2^31 <= n <= 2^31-1
+ * n is an integer.
+ * Either x is not zero or n > 0.
+ * -10^4 <= x^n <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n==0)
+            return 1;
+
+        auto res = myPow(x, n/2);
+        res *= res;
+        if (n<0) x = 1/x;
+        if (n%2) res *= x;
+
+        return res;
+    }
+};
+// @lc code=end
+
