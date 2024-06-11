e9eed268f21bb06dbe365fee32117cc6225fdf29
Rahul Senna <rahulvsenna@gmail.com>
2024-06-11 12:03:58 +0530

    https://codeforces.com/contest/1980/problem/A

---
+++ codeforces_solved/A_Problem_Generator.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if 1
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": "; _debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _debug() {cerr << '\n';}
+template<typename T, typename... Args>
+void _debug(const T& first, const Args&... args) { cerr << " "; _print(first); cerr << " | "; _debug(args...);}
+#endif 
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    int t,n_problem, n_rounds;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n_problem >> n_rounds;
+        unordered_map<char,int> problems_bucket;
+
+        while(n_problem--)
+        {
+            char c; cin >> c;
+            problems_bucket[c]++;
+        }
+        int res = 0;
+        for (char c='A'; c<='G'; c++)
+            res += min(0, problems_bucket[c] - n_rounds);
+        
+        cout << abs(res) << '\n';
+    }
+ 
+    return 0;
+}
