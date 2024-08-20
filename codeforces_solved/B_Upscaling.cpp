81dcf15867f864010f31af3de2099d7e2f21a107
Rahul Senna <rahulvsenna@gmail.com>
2024-08-20 22:37:12 +0530

    https://codeforces.com/contest/1950/problem/B

---
+++ codeforces_solved/B_Upscaling.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": "; _setupvar(#__VA_ARGS__); _debug(__VA_ARGS__);
+void _print(s64 t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(r64 t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(u64 t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _debug() {cerr << '\n';}
+vector<string> VARS;int VAR_IDX = 0;
+void _setupvar(string v) {VARS = std::views::split(v, ',') | ranges::to<vector<string>>();VAR_IDX = 0;}
+
+template<typename T, typename... Args>
+void _debug(const T& first, const Args&... args) { cerr << VARS[VAR_IDX++] << ": "; _print(first); cerr << ", "; _debug(args...);}
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    s64 t, n;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        s64 n2 = n*2;
+        std::vector<std::vector<char>> grid(2*n, std::vector<char>(2*n, '#'));
+
+        for (s64 y = 0; y < n2; y+=2)
+        {
+            for (s64 x = 0; x < n2; x+=2)
+            {
+                s64 yy = y/2;
+                s64 xx = x/2;
+
+                if ((yy%2 ==0) ^ (xx%2 ==0))
+                {
+                    grid[x  ][y  ] = '.';
+                    grid[x+1][y  ] = '.';
+                    grid[x  ][y+1] = '.';
+                    grid[x+1][y+1] = '.';
+                }
+            }
+        }
+        
+        for (auto &e: grid)
+        {
+        	cout << std::string(e.begin(), e.end()) << '\n';
+        }
+    }
+ 
+    return 0;
+}
