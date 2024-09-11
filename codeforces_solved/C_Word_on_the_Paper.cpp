8ed73b3edf1c6e39a9d55f891a6b38d4ccbaef4d
Rahul Senna <rahulvsenna@gmail.com>
2024-09-11 14:39:14 +0530

    https://codeforces.com/contest/1850/problem/C

---
+++ codeforces_solved/C_Word_on_the_Paper.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    s64 t;
+    cin >> t;
+
+    char c;
+    while(t--)
+    {
+        string res = "";
+        for (int y = 0; y < 8; ++y)
+        {
+            for (int x = 0; x < 8; ++x)
+            {
+                cin >> c;
+                if (c != '.')
+                {
+                    res += c;
+                }
+            }
+        }
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
