15b27ffb56abc90f19d9311c0af742d1bcaa58ce
Rahul Senna <rahulvsenna@gmail.com>
2024-09-13 00:36:46 +0530

    https://codeforces.com/contest/1829/problem/A

---
+++ codeforces_solved/A_Love_Story.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    s64 t;
+    cin >> t;
+    string ss = "codeforces";
+    while(t--)
+    {
+        int res = 0;
+        string s;
+        cin >> s;
+        for (int i = 0; i < 10; ++i)
+        {
+        	 if (s[i] != ss[i])
+                 res++;
+        }
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
