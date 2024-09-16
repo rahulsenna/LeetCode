0553ede96e276bb988ae64d32fbdc6f65268e9ef
Rahul Senna <rahulvsenna@gmail.com>
2024-09-17 00:47:47 +0530

    https://codeforces.com/contest/1829/problem/D

---
+++ codeforces_solved/D_Gold_Rush.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+bool bucketable(s64 n, s64 m)
+{
+    if (m==n)
+    {
+        return true;
+    }
+    if (m>n || n%3!=0)
+    {
+        return false;
+    }
+    s64 a = n/3;
+    s64 b = n-a;
+
+    return bucketable(a, m) | bucketable(b, m);
+}
+
+int main()
+{
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+    s64 t,n,m;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n >> m;
+
+        if (bucketable(n,m))
+            std::cout << "YES" << '\n';
+        else
+            std::cout << "NO" << '\n';
+
+    }
+ 
+    return 0;
+}
