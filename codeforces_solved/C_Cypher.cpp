746d7fd145ed8b96846712419d83b16b7034b041
Rahul Senna <rahulvsenna@gmail.com>
2024-10-10 13:11:17 +0530

    https://codeforces.com/contest/1703/problem/C

---
+++ codeforces_solved/C_Cypher.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t,n;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        std::vector<int> pos(n);
+        for (auto &e: pos)
+        {
+        	cin >> e;
+        }
+        int NN;
+        for (int i = 0; i < n; ++i)
+        {
+        	 cin >> NN;
+             char c;
+             int tot = 0;
+             while(NN--)
+             {
+                cin >> c;
+                if (c=='U')
+                    tot--;
+                else
+                    tot++;
+             }
+         
+             pos[i] = (pos[i]+tot)%10;
+             if (pos[i]<0)             
+                pos[i] += 10;
+        }
+
+        for (auto &e: pos)
+        {
+        	cout << e << " ";
+        }
+        cout << '\n';
+    }
+ 
+    return 0;
+}
+// https://codeforces.com/contest/1703/problem/C
