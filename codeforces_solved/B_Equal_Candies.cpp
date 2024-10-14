a0a5441f9277d37da431ab3cdc704c9103b16114
Rahul Senna <rahulvsenna@gmail.com>
2024-10-14 16:19:22 +0530

    https://codeforces.com/contest/1676/problem/B

---
+++ codeforces_solved/B_Equal_Candies.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t, n, e;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        s64 minx = LONG_MAX;
+        s64 total = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> e;
+            total += e;
+            minx = min(e, minx);
+        }
+
+        total -= (minx * n);
+        std::cout << total << '\n';
+    }
+ 
+    return 0;
+}
+
+// https://codeforces.com/contest/1676/problem/B
