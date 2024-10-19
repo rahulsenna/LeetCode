6229e8ef4dc201729a6610f449aa7d085d43f1c5
Rahul Senna <rahulvsenna@gmail.com>
2024-10-19 12:35:01 +0530

    https://codeforces.com/contest/1692/problem/A

---
+++ codeforces_solved/A_Marathon.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t;
+    cin >> t;
+    std::vector<int> p(4);
+
+    while(t--)
+    {
+        int a;
+        cin >> a;
+        int res = 0;
+        for (int i = 0; i < 3; ++i)
+        {
+            int e;
+        	cin >> e;
+            if (e>a)
+                res++;
+        }
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
+// https://codeforces.com/contest/1692/problem/A
