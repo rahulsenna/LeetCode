63d6be3629607ff4e585dc71fb26bb7cb7df16bf
Rahul Senna <rahulvsenna@gmail.com>
2024-10-22 01:22:21 +0530

    https://codeforces.com/contest/1669/problem/B

---
+++ codeforces_solved/B_Triple.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t, n;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        vector<int> mp(n+1,0);
+        int res = -1;
+        int e;
+        while(n--)
+        {
+            cin >> e;
+        	mp[e]++;
+            if (mp[e]>2) res = e;
+        }
+
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
+// https://codeforces.com/contest/1669/problem/B
