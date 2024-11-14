ea798523bb734141480be584567abf05e660bbed
Rahul Senna <rahulvsenna@gmail.com>
2024-11-15 02:15:16 +0530

    https://codeforces.com/contest/2000/problem/C

--- codeforces_solved/C_Numeric_String_Template.cpp
+++ codeforces_solved/C_Numeric_String_Template.cpp
@@ -34,39 +34,41 @@ int main()
 
     while(t--) 
     {
-    cin >> n;
-    vector<s64> nums(n);
-    for (auto &e: nums) cin >> e;
-    
-    cin >> m;
-    string s;
-    while(m--) {
-        cin >> s;
-        if (s.size() != n) {
-            cout << "NO\n";
-            continue;
-        }
+        cin >> n;
+        vector<s64> nums(n);
+        for (auto& e : nums) cin >> e;
 
-        unordered_map<s64, char> num_to_char;
-        unordered_map<char, s64> char_to_num;
-        bool valid = true;
-
-        for (int i = 0; i < n && valid; ++i) {
-            char c = s[i];
-            s64 num = nums[i];
-
-            if (!num_to_char.count(num) && !char_to_num.count(c)) {
-                num_to_char[num] = c;
-                char_to_num[c] = num;
+        cin >> m;
+        string s;
+        while (m--) {
+            cin >> s;
+            if (s.size() != n) 
+            {
+                cout << "NO\n";
+                continue;
             }
-            else if (num_to_char[num] != c || 
-                     (char_to_num.count(c) && char_to_num[c] != num)) {
-                valid = false;
-            }
-        }
 
-        cout << (valid ? "YES\n" : "NO\n");
-    }
+            unordered_map<s64, char> num_to_char;
+            unordered_map<char, s64> char_to_num;
+            bool valid = true;
+
+            for (int i = 0; i < n && valid; ++i)
+            {
+                char c = s[i];
+                s64 num = nums[i];
+
+                if (!num_to_char.count(num) && !char_to_num.count(c)) {
+                    num_to_char[num] = c;
+                    char_to_num[c] = num;
+                }
+                else if (num_to_char[num] != c ||
+                    (char_to_num.count(c) && char_to_num[c] != num)) {
+                    valid = false;
+                }
+            }
+
+            cout << (valid ? "YES\n" : "NO\n");
+        }
 }
  
     return 0;
