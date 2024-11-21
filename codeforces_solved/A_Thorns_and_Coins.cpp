dc3d2a1203d76129a61e45f48ecfb6c06223f3bf
Rahul Senna <rahulvsenna@gmail.com>
2024-11-21 20:10:15 +0530

    https://codeforces.com/contest/1932/problem/A

---
+++ codeforces_solved/A_Thorns_and_Coins.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t,n;
+    cin >> t;
+    string s;
+    while(t--)
+    {
+        cin >> n >> s;
+
+        int res = 0;
+        char prev = '.';
+        for (auto c: s)
+        {
+
+        	if (c == '@')
+                res++;
+
+            if (c=='*' and prev == '*')
+                break;
+                prev = c;
+        }
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
+
+// https://codeforces.com/contest/1932/problem/A
