180e4d08c9fc42729aeebfa32b54f42377e35c2b
Rahul Senna <rahulvsenna@gmail.com>
2024-12-20 17:38:59 +0530

    https://codeforces.com/contest/2044/problem/C

---
+++ codeforces_solved/C_Hard_Problem.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef long double r64;
+
+inline void setup() {
+    fastio;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout), freopen("error.txt", "w", stderr);
+#endif
+}
+
+#ifndef ONLINE_JUDGE
+#include "debug_template.h"
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+    setup();
+
+    s64 t,m,a,b,c;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> m >> a >> b >> c;
+
+        s64 aa = m, bb = m, cc = 0;
+        s64 res = 0;
+        auto mina = min(m,a);
+        aa -= mina;
+        cc += aa;
+        res += mina;
+        auto minb = min(m,b);
+        bb -= minb;
+        cc += bb;
+        res += minb;
+        auto minc = min(cc,c);
+        res += minc;
+
+        std::cout << res << '\n';
+    }
+ 
+    return 0;
+}
+// https://codeforces.com/contest/2044/problem/C
